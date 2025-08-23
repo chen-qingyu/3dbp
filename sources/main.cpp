41de16159ca24ebe81659a1ccb28fec6bb34158e
chen-qingyu <chen_qingyu@qq.com>
2025-08-23 19:12:49 +0800

    ✨ feat: use cmd arg to read file

--- sources/main.cpp
+++ sources/main.cpp
@@ -6,14 +6,20 @@
 
 #include "algorithm.hpp"
 
+using std::filesystem::path;
+
 int main(int argc, char** argv)
 {
-    std::string file = "demo.json";
-    std::string input_dir = "data/";
-    std::string output_dir = "result/";
+    if (argc != 2)
+    {
+        spdlog::error("Usage: {} <input_json>", path(argv[0]).filename().string());
+        return 1;
+    }
+
+    std::string file = argv[1];
 
     // 读取输入
-    std::ifstream input(input_dir + file);
+    std::ifstream input(file);
     if (!input.is_open())
     {
         spdlog::error("Failed to open input file \"{}\".", file);
@@ -27,11 +33,10 @@ int main(int argc, char** argv)
     Output output_data = Simple(input_data).run();
 
     // 输出结果到文件
-    std::filesystem::create_directories(output_dir);
-    std::ofstream output(output_dir + "result-" + file);
-    output << json(output_data).dump(2);
-    output.close();
-    spdlog::info("Results written to \"{}\".", output_dir + "result-" + file);
+    path output = path("result") / ("result-" + path(file).filename().string());
+    std::filesystem::create_directories(output.parent_path());
+    std::ofstream(output) << json(output_data).dump(2);
+    spdlog::info("Results written to \"{}\".", output.string());
 
     return 0;
 }
